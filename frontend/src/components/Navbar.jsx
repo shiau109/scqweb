5add9502ee14064287a0cd4ae2b73a55460af146
shiau109 <shiau109@gmail.com>
2025-04-24 23:53:51 +0800

    Update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,17 +5,11 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="sticky top-0 z-50 bg-white border-b border-gray-200 dark:bg-gray-800">
+    <nav className="sticky top-0 z-50 bg-white border-b border-gray-200 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path
-              fillRule="evenodd"
-              d="M3 5h14a1 1 0 010 2H3a1 1 0 110-2zm0 4h14a1 1 0 010 2H3a1 1 0 110-2zm0 4h14a1 1 0 010 2H3a1 1 0 110-2z"
-              clipRule="evenodd"
-            />
-          </svg>
+        <p className="mt-2 text-gray-700">This is the navigator bar.</p>
+
       </div>
     </nav>
   );
