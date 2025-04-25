35d88f4d48823845b8647518af86ec6963530817
shiau109 <shiau109@gmail.com>
2025-04-25 19:11:34 +0800

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,11 @@ const Home = () => {
         <div className="p-4">
           <h1 className="text-2xl font-bold">Welcome to SCQWeb</h1>
           <p className="mt-2 text-gray-700">This is the homepage content.</p>
+          <div className="p-4 bg-green-500 text-white">
+            ✅ If you see me green, Tailwind is working!
+            </div>
         </div>
+        
       );
 };
 
